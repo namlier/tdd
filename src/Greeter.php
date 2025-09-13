5a2356617b4b3dfeae5de5906a5506ff13394fd7
namlier <namliers@gmail.com>
2025-09-13 21:24:57 +0300

    refactored using advantages of TDD

--- src/Greeter.php
+++ src/Greeter.php
@@ -8,11 +8,11 @@ final class Greeter
 {
     public function greet(?string $greetWord = null): string
     {
-        $greetWord = $greetWord ?? 'World';
-
-        if (mb_strlen($greetWord) > 16) {
+        if (isset($greetWord) && mb_strlen($greetWord) > 16) {
             throw new \Exception('Greet word is too long!');
         }
+
+        $greetWord = $greetWord ?? 'World';
 
         return "Hello {$greetWord}!";
     }
