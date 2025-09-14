dc84beb2a40a9ba39bcbcd41fda8a493265b769e
namlier <namliers@gmail.com>
2025-09-14 23:47:16 +0300

    provided new PasswordValidator TDD is awesome

--- src/TestDox/PasswordValidator.php
+++ src/TestDox/PasswordValidator.php
@@ -6,8 +6,42 @@ namespace Namlier\UnitTesting\TestDox;
 
 class PasswordValidator
 {
+    private const string SPECIAL_SYMBOLS = '!@#$%^&*()';
+
     public function isValid(string $password): bool
     {
-        return mb_strlen($password) >= 8;
+        $message = '';
+        $hasError = false;
+
+        if (mb_strlen($password) < 8) {
+            $hasError = true;
+            $message.= 'Password should contain at least 8 symbols.';
+        }
+
+        if (preg_match('/[A-Z]/', $password) !== 1) {
+            $hasError = true;
+            $message.= 'Password should contain at least one uppercase character.';
+        }
+
+        if (preg_match('/[a-z]/', $password) !== 1) {
+            $hasError = true;
+            $message.= 'Password should contain at least one lowercase character.';
+        }
+
+        if (preg_match('/\d/', $password) !== 1) {
+            $hasError = true;
+            $message.= 'Password should contain at least one number.';
+        }
+
+        if (preg_match('/[!@#$%^&*()]/', $password) !== 1) {
+            $hasError = true;
+            $message.= 'Password should contain at least one special symbol from list "' . self::SPECIAL_SYMBOLS . '".';
+        }
+
+        if ($hasError) {
+            throw new \Exception($message);
+        }
+
+        return true;
     }
 }
--- tests/unit/TestDox/PasswordValidatorTest.php
+++ tests/unit/TestDox/PasswordValidatorTest.php
@@ -10,24 +10,102 @@ use PHPUnit\Framework\TestCase;
 
 class PasswordValidatorTest extends TestCase
 {
-    #[DataProvider('minimum8SymbolsProvider')]
-    public function testIsValidWhenMinimum8Symbols($password, $expected, $messageOnFail): void
+    private const string SPECIAL_SYMBOLS = '!@#$%^&*()';
+
+    /**
+     * Array count is 8.
+     */
+    private const array GOOD_PASSWORD_CHARACTERS_EXAMPLE = [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        'uppercase' => 'N',
+        'lowercase' => 'n',
+        'special_symbol' => '@'
+    ];
+
+    public function testIsNotValidWhenThereAreLessThan8Symbols(): void
     {
         $sut = new PasswordValidator();
 
-        $result = $sut->isValid($password);
+        $goodPasswordArray = self::GOOD_PASSWORD_CHARACTERS_EXAMPLE;
+        $badPasswordArray = array_slice($goodPasswordArray, 0, 7);
 
-        $this->assertEquals($expected, $result, $messageOnFail);
+        if (count($badPasswordArray) >= 8) {
+            $this->fail('It is important for the test to have less than 8 symbols in provided password');
+        }
+
+        $notEnoughCharactersPassword = implode($badPasswordArray);
+
+        self::expectException(\Exception::class);
+        self::expectExceptionMessage('Password should contain at least 8 symbols.');
+
+        $sut->isValid($notEnoughCharactersPassword);
     }
 
-    public static function minimum8SymbolsProvider(): array
+    public function testIsNotValidWhenNoAnyUppercaseCharProvided(): void
     {
-        return [
-            '7 symbols' => ['1234567', false, 'It should be not valid when there is less than 8 symbols'],
-            '0 symbols' => ['', false, 'It should be not valid when there are no symbols'],
-            '8 symbols' => ['12345678', true, 'It should be valid when there are 8 symbols'],
-            '9 symbols' => ['123456789', true, 'It should be valid when there are more than 8 symbols'],
-            '16 symbols' => ['1234567890123456', true, 'It should be valid when there are more than 8 symbols'],
-        ];
+        $sut = new PasswordValidator();
+
+        $goodPasswordArray = self::GOOD_PASSWORD_CHARACTERS_EXAMPLE;
+        $badPasswordArray = array_filter($goodPasswordArray, fn ($value, $key) => $key !== 'uppercase', ARRAY_FILTER_USE_BOTH);
+        array_push($badPasswordArray, 'n');
+
+        $noUpperCasePassword = implode($badPasswordArray);
+
+        self::expectException(\Exception::class);
+        self::expectExceptionMessage('Password should contain at least one uppercase character.');
+
+        $sut->isValid($noUpperCasePassword);
+    }
+
+    public function testIsNotValidWhenNoAnyLowercaseCharProvided(): void
+    {
+        $sut = new PasswordValidator();
+
+        $goodPasswordArray = self::GOOD_PASSWORD_CHARACTERS_EXAMPLE;
+        $badPasswordArray = array_filter($goodPasswordArray, fn ($value, $key) => $key !== 'lowercase', ARRAY_FILTER_USE_BOTH);
+        array_push($badPasswordArray, 'N');
+
+        $noLowerCasePassword = implode($badPasswordArray);
+
+        self::expectException(\Exception::class);
+        self::expectExceptionMessage('Password should contain at least one lowercase character.');
+
+        $sut->isValid($noLowerCasePassword);
+    }
+
+    public function testIsNotValidWhenNoAnyNumberProvided(): void
+    {
+        $sut = new PasswordValidator();
+
+        $goodPasswordArray = self::GOOD_PASSWORD_CHARACTERS_EXAMPLE;
+        $badPasswordArray = array_slice($goodPasswordArray, 5);
+        array_push($badPasswordArray, 'q', 'w', 'e', 'r', 't ');
+
+        $noAnyNumberPassword = implode($badPasswordArray);
+
+        self::expectException(\Exception::class);
+        self::expectExceptionMessage('Password should contain at least one number.');
+
+        $sut->isValid($noAnyNumberPassword);
+    }
+
+    public function testIsNotValidWhenNoAnySpecialSymbolProvided(): void
+    {
+        $sut = new PasswordValidator();
+
+        $goodPasswordArray = self::GOOD_PASSWORD_CHARACTERS_EXAMPLE;
+        $badPasswordArray = array_filter($goodPasswordArray, fn ($value, $key) => $key !== 'special_symbol', ARRAY_FILTER_USE_BOTH);
+        array_push($badPasswordArray, 'N');
+
+        $noAnySpecialSymbolPassword = implode($badPasswordArray);
+
+        self::expectException(\Exception::class);
+        self::expectExceptionMessage('Password should contain at least one special symbol from list "' . self::SPECIAL_SYMBOLS . '".');
+
+        $sut->isValid($noAnySpecialSymbolPassword);
     }
 }
