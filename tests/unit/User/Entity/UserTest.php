5b634d39d894c295568a697ff061b9e68a08e7f7
namlier <namliers@gmail.com>
2025-10-08 14:53:36 +0300

    add User entity unit test, to ensure default role is given.

---
+++ tests/unit/User/Entity/UserTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+use Namlier\UnitTesting\User\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testCreateNewUserGetsDefaultRole(): void
+    {
+        $sut = new User('johndoe@gmail.com', '123123q');
+
+        self::assertEquals('ROLE_USER', $sut->getRole(), 'User should get default role during creation.');
+    }
+}
